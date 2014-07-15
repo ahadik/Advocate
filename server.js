8c9faabffe0bdd21c96c63a7b1a8cd9624efe3e8
Alexander Hadik <alexander_hadik@brown.edu>
2014-07-14 21:56:30 -0500

    removing outdated code for storing form submissions in memory

--- server.js
+++ server.js
@@ -14,38 +14,26 @@ var server = http.createServer(function(req, res){
 		path = join(root, '/index.html');
 	}
 	
-	if(url.pathname == '/submits'){
-		res.writeHead(200, {"Content-Type": "text/plain"});
-		
-		var data = "";
-		for(entry in entries){
-			data+=entries[entry].orgType+", "+entries[entry].orgName+", "+entries[entry].email+"\n";
-		} 
-		
-		res.write(data);
-		res.end();
-	}else{	
-		fs.stat(path, function(err, stat){
-			if(err){
-				
-				if('ENOENT' == err.code) {
-					res.statusCode = 404;
-					res.end('Not Found');
-				}else{
-					res.statusCode = 500;
-					res.end('Internal Server Error');
-				}
-			} else{
-				res.setHeader('Content-Length', stat.size);
-				var stream = fs.createReadStream(path);
-				stream.pipe(res);
-				stream.on('error', function(err){
-					res.statusCode = 500;
-					res.end('Internal Server Error');
-				});
+	fs.stat(path, function(err, stat){
+		if(err){
+			
+			if('ENOENT' == err.code) {
+				res.statusCode = 404;
+				res.end('Not Found');
+			}else{
+				res.statusCode = 500;
+				res.end('Internal Server Error');
 			}
-		});
-	}
+		} else{
+			res.setHeader('Content-Length', stat.size);
+			var stream = fs.createReadStream(path);
+			stream.pipe(res);
+			stream.on('error', function(err){
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			});
+		}
+	});
 });
 
 server.listen(port, function(){
