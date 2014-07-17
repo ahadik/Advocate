29270206ead30cbd170b7404daaa9f7373ee9cd0
Alexander Hadik <alexander_hadik@brown.edu>
2014-07-16 23:23:45 -0500

    Adding final code and styling for advocate interest form backend

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var join = require('path').join;
 var fs = require('fs');
 var port = Number(process.env.PORT || 5000);
 var root = __dirname;
+var formServer = require('./lib/form_server');
 var entries = [];
 
 
@@ -14,31 +15,58 @@ var server = http.createServer(function(req, res){
 		path = join(root, '/index.html');
 	}
 	
-	fs.stat(path, function(err, stat){
-		if(err){
-			
-			if('ENOENT' == err.code) {
-				res.statusCode = 404;
-				res.end('Not Found');
-			}else{
-				res.statusCode = 500;
-				res.end('Internal Server Error');
+	if(url.pathname == '/submits'){
+		res.statusCode = 200;
+		res.setHeader('Content-Type:', 'text/html');
+		formServer.report(function(data){
+		
+			var dataHTML = "<html>" +
+							"<head>"+
+							"<title>Advocate Form Submissions</title>"+
+							"<link rel=\"stylesheet\" href=\"public/backend_style.css\" title=\"Advocate Backend Styling\" type=\"text/css\" media=\"screen\" charset=\"utf-8\">"+
+							"</head>" +
+							"<body>"+
+							"<h1>Advocate</h1>"+
+							"<h2>Interest Form Submissions</h2>"+
+							"<table><thead><tr><td>Organization Type</td><td>Organization Name</td><td>Contact Email</td></tr></thead><tbody>";
+			for (row in data){
+				dataHTML+="<tr><td>"+data[row].orgtype+"</td>";
+				dataHTML += "<td>"+data[row].orgname+"</td>";
+				dataHTML += "<td><a href=\"mailto:"+data[row].email+"\">"+data[row].email+"</a></td></tr>";
 			}
-		} else{
-			res.setHeader('Content-Length', stat.size);
-			var stream = fs.createReadStream(path);
-			stream.pipe(res);
-			stream.on('error', function(err){
-				res.statusCode = 500;
-				res.end('Internal Server Error');
-			});
-		}
-	});
+			
+			dataHTML+="</tbody></table>"+
+						"</body>"+
+						"</html>";
+						
+			res.end(dataHTML);
+		});
+	}else{	
+		fs.stat(path, function(err, stat){
+			if(err){
+				
+				if('ENOENT' == err.code) {
+					res.statusCode = 404;
+					res.end('Not Found');
+				}else{
+					res.statusCode = 500;
+					res.end('Internal Server Error');
+				}
+			} else{
+				res.setHeader('Content-Length', stat.size);
+				var stream = fs.createReadStream(path);
+				stream.pipe(res);
+				stream.on('error', function(err){
+					res.statusCode = 500;
+					res.end('Internal Server Error');
+				});
+			}
+		});
+	}
 });
 
 server.listen(port, function(){
 	console.log('Listening on port: ' + port);
 });
 
-var formServer = require('./lib/form_server');
-formServer.listen(server, entries);
+formServer.listen(server);
