181b7ac34aaee1fd35b54204d3a25fe82dfff67e
Alexander Hadik <alexander_hadik@brown.edu>
2014-09-11 08:56:25 -0400

    Changed account complete redirect to home page

--- app.js
+++ app.js
@@ -46,27 +46,27 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 	var events = db.collection('events');
 	var users = db.collection('users');
 	var eventSubmit = db.collection('eventSubmit');
-	
+
 	organizations.find({}).toArray(function(err, orgs){
 		for(org in orgs){
 			accountIDs[orgs[org].orgID] = 1;
 		}
 	});
-	
+
 	notifs.find({}).toArray(function(err, notifs){
 		for(notif in notifs){
 			notifIDs[notifs[notif].notifID] = 1;
 		}
 	});
-	
+
 	var app = express();
-	
+
 	// all environments
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/views');
 	app.use(express.static(__dirname + '/public'));
 	app.set('view engine', 'ejs');
-	
+
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
@@ -82,24 +82,24 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 	app.use(express.static(path.join(__dirname, '')));
 	app.use(express.favicon());
 	app.use(express.errorHandler());
-	
-	
+
+
 	// mongoose
 	mongoose.connect(process.env.MONGOHQ_DB);
-	
+
 	require('./config/passport')(passport, accountIDs, notifIDs, users, userData, notifs); // pass passport for configuration
-	
+
 	app.set('photos', __dirname + '/public/photos');
-	
+
 	// development only
 	if ('development' == app.get('env')) {
 	  app.use(express.errorHandler());
 	}
-	
+
 	app.get('/', function(req, res){
 		index.index(req, res, userData, organizations, notifs);
 	});
-	
+
 	app.get('/style', function(req, res){
 		if(req.isAuthenticated()){
 			console.log(req.user);
@@ -108,7 +108,7 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 			res.render('generic', {auth : false});
 		}
 	});
-	
+
 	app.get('/clear', function(req, res){
 		var users = db.collection('users');
 		users.remove({}, function(err, docs){});
@@ -119,18 +119,18 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 	app.get('/submits', registrations.form);
 	app.get('/register', register.form);
 	app.get('/auth', register.twitter);
-	
+
 	app.post('/register', passport.authenticate('local-signup', {
 		successRedirect : '/profileComplete', // redirect to the secure profile section
 		failureRedirect : '/register', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
-	
+
 	app.post('/complete', function(req, res){
 		if(err){console.log(err);}
 		var accountUsername = req.user.username;
 
-		//If the zipcode field has a value, then the city, state, lat and long need to be recomputed and updated in the database			
+		//If the zipcode field has a value, then the city, state, lat and long need to be recomputed and updated in the database
 		if(req.body.zipCode != ''){
 			geo.createLocationfromZip(req.body.zipCode, function(location){
 				userData.update(
@@ -154,7 +154,7 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 				});
 				userData.find({username : accountUsername}).toArray(function(err, users){
 					console.log("Account creation complete. Rendering account page");
-					res.redirect('/account');
+					res.redirect('/');
 				});
 			});
 		//otherwise, only the interests section needs to be updated
@@ -178,12 +178,12 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 			});
 		}
 	});
-	
+
 	app.get('/login', function(req, res){
 		res.render('login', {user : req.user});
 	});
-	
-	
+
+
 	app.post('/login', function(req,res, next){
 		passport.authenticate('local-login', function(err, user, info){
 			if (err){return next(err);}
@@ -196,66 +196,66 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 				}else{
 					return res.redirect('/style');
 				}
-				
+
 			});
 		})(req,res,next);
 	});
-	
+
 //exports.updateAccount = function(data, notifIDs, callback, users, accountData, notifs){
-	
+
 	app.get('/logout', function(req, res) {
 		req.logout();
 		res.redirect('/');
 	});
-	
+
 	app.get('/account', function(req, res){
 		account.accountView(req, res, userData, notifs);
 	});
-	
+
 	app.get('/switch', function(req,res){
 		account.switchAccounts(req, res, userData, organizations);
 	});
-	
+
 	app.get('/newOrganization', function(req, res){
 		account.createOrg(req, res, userData, organizations, notifs);
 	});
-	
+
 	app.get('/newEvent', function(req, res){
 		account.createEvent(req, res, systemData, userData, organizations, notifs);
 	});
-	
+
 	app.get('/profileComplete', function(req, res){
 		account.accountComplete(req, res, systemData, userData, notifs);
 	});
-	
+
 	app.get('/eventView', function(req, res){
 		account.eventView(req, res, userData, notifs, events, organizations);
 	});
-	
+
 	app.get('/sign_s3', function(req,res){
 		upload.sign(req, res, process.env.S3_PROFILE_BUCKET)
 	});
-	
+
 	app.get('/sign_orglogo', function(req, res){
 		upload.sign(req, res, process.env.S3_ORG_BUCKET)
 	});
-	
+
 	app.get('/sign_eventcover', function(req, res){
 		upload.sign(req, res, process.env.S3_EVENT_BUCKET)
 	});
-	
+
 	app.post('/submit_form', upload.submit_form);
-	
+
 	app.post('/newOrganization', function(req,res){
 		orgs.newOrg(req,res, accountIDs, notifIDs, organizations, userData, notifs);
 	});
-	
+
 	app.post('/newEvent', function(req, res){
 		orgs.newEvent(req, res, events, userData);
 	});
-	
+
 	app.get('/auth/twitter', passport.authenticate('twitter'));
-	
+
 	// handle the callback after twitter has authenticated the user
 	app.get('/auth/twitter/callback',
 		passport.authenticate('twitter', {
@@ -263,18 +263,18 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 			failureRedirect : '/login'
 		})
 	);
-	
+
 	//TEMP EVENT CREATE FORM
 	app.post('/create', function(req, res){
 		orgs.tempEvent(req, res, eventSubmit);
 	});
-	
+
 	app.get('/create', function(req, res){
 		res.render('event_temp', {auth : false});
 	});
-	
+
 	/*-----------------------DON'T GO BELOW THIS--------------------*/
-	
+
 	app.use(function(req, res, next){
 		if(req.isAuthenticated()){
 			account.renderProfilePages('404', req, res, {auth : true}, userData, organizations, notifs);
@@ -282,13 +282,13 @@ MongoClient.connect(process.env.MONGOHQ_DB, function(err, db) {
 			res.render('404', {auth : false});
 		}
 	});
-	
+
 	var server = http.createServer(app).listen(app.get('port'), function(){
 	  console.log('Express server listening on port ' + app.get('port'));
 	});
-	
+
 	var formServer = require('./lib/form_server');
-	formServer.listen(server, socketio, express, sessionStore, userData);	
+	formServer.listen(server, socketio, express, sessionStore, userData);
 });
 
 // route middleware to make sure a user is logged in
--- public/javascripts/js_generic.js
+++ public/javascripts/js_generic.js
@@ -2,19 +2,32 @@ var toggleIndicator = 0;
 
 
 $(".overlay").hide();
+$("#register").hide();
+$("#login").hide();
 
 function preventHide(){
 	toggleIndicator=2;
 }
 
-function toggleDialogue(){
+function toggleDialogue(modal){
 	if(!toggleIndicator){
-		$( ".overlay" ).fadeIn( "slow");
+		$( ".overlay"+modal ).fadeIn( "slow");
 		toggleIndicator=1;
 	}else if(toggleIndicator==2){
 		toggleIndicator=1;
 	}else{
-		$( ".overlay" ).fadeOut( "slow");
+		$( ".overlay"+modal ).fadeOut( "slow");
+		$('#register').fadeOut("slow");
 		toggleIndicator=0;
 	}
+}
+
+function registerModal(){
+	toggleDialogue('#registerOver');
+	$('#register').fadeIn("slow");
+}
+
+function loginModal(){
+	toggleDialogue();
+	$('#login').fadeIn("slow");
 }
